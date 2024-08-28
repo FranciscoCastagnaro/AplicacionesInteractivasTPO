2d1998a36f78ce04608775423d1af397c86b3f8c
FranciscoCastagnaro <francastagnaro@hotmail.com>
2024-08-28 10:51:08 -0300

    first commit

---
+++ src/main/java/com/UADE/TEST/demo/TpoApIsTestApplication.java
@@ -0,0 +1,13 @@
+package com.UADE.TEST.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpoApIsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpoApIsTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/UADE/TEST/demo/TpoApIsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UADE.TEST.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpoApIsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
